73b6b2d7754c83c471754d3c014fe0da70716c83
Joshua-M1 <70333870+Joshua-M1@users.noreply.github.com>
2021-03-22 11:16:31 -0400

    Update Play.java

--- src/neoncontrol/Play.java
+++ src/neoncontrol/Play.java
@@ -1,35 +1,81 @@
 package neoncontrol;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import javafx.scene.control.*;
+import javafx.scene.input.*;
+import javafx.animation.*;
+import javafx.scene.*;
 
-/**
- *
- * @author addav
- */
-public class Play {
+public class Play{
     private Level level;
     private PhysicsEngine physics;
-    
+    private StickSpring ss;
+    private Scene keyChecker;
+    boolean collided = false;
+    private AnimationTimer gameTimer = new AnimationTimer() {
+        @Override
+        public void handle(long l) {
+            collided = false;
+            ss.getHB1().setY(ss.getHB1().getY() + ss.getVelocityVec().getY());
+            ss.getHB1().setX(ss.getHB1().getX() + ss.getVelocityVec().getX());
+            ss.getHB2().setY(ss.getHB2().getY() + ss.getVelocityVec().getY());
+            ss.getHB2().setX(ss.getHB2().getX() + ss.getVelocityVec().getX());
+            ss.getHB3().setY(ss.getHB3().getY() + ss.getVelocityVec().getY());
+            ss.getHB3().setX(ss.getHB3().getX() + ss.getVelocityVec().getX());
+
+            level.getWallList().forEach((wall) -> {
+                
+                if(ss.getHB1().intersects(wall.getHB().getBoundsInLocal()) && !collided){
+                    ss.setVelocityVec(physics.collisionSpring(ss.getVelocityVec(), ss.getAngle()));
+                    collided = true;
+                }
+                else if(ss.getHB2().intersects(wall.getHB().getBoundsInLocal()) && !collided){
+                    ss.setVelocityVec(physics.collisionSpring(ss.getVelocityVec(), ss.getAngle() + 180));
+                    collided = true;
+                }
+                else if(ss.getHB3().intersects(wall.getHB().getBoundsInLocal()) && !collided){
+                    ss.setVelocityVec(physics.collisionSide(ss.getVelocityVec(), wall));
+                    collided = true;
+                }
+            });
+            if(!collided)
+                ss.setVelocityVec(physics.calculateMove(ss.getVelocityVec()));
+            
+            ss.setYPos(ss.getVelocityVec().getY() + ss.getYPos());
+            ss.setXPos(ss.getVelocityVec().getX() + ss.getXPos());
+            
+            ss.getHB1().setY(ss.getYPos() + 100);
+            ss.getHB1().setX(ss.getXPos() + 9);
+            ss.getHB2().setY(ss.getYPos());
+            ss.getHB2().setX(ss.getXPos() + 9);
+            ss.getHB3().setY(ss.getYPos() + 20);
+            ss.getHB3().setX(ss.getXPos() + 9);
+        }
+    };
+
     public Play(){
         
     }
     
-    public Play(Level level, PhysicsEngine physics){
+    public Play(Level level, PhysicsEngine physics, StickSpring ss, Scene keyChecker){
         this.level = level;
         this.physics = physics;
-    }
+        this.ss = ss;
+        this.keyChecker = keyChecker;
+    }    
     
     public void start(){
-        //Start running the update method
+        gameTimer.start();
+        keyChecker.setOnKeyPressed((KeyEvent e) -> {
+            switch (e.getCode()){
+                case ESCAPE: gameTimer.stop();break;
+            }
+        });
     }
     
-//    public void showMenu(Menu menu){
-//        
-//    }
+    public void showMenu(Menu menu){
+        
+    }
+
     
     public void setPhysics(PhysicsEngine physics){
         this.physics = physics;
